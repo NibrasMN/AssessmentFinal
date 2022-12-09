4ef2ad89b399b8151a96a5c0c4b2ab6be1247558
NibrasMN <114565621+NibrasMN@users.noreply.github.com>
2022-12-09 15:21:22 +0530

    Add files via upload

---
+++ q50/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MagicNumberComp from './MagicNumberComp';
+import {Provider} from 'react-redux'
+import store from './redux/store';
+
+
+function App() {
+
+  return (
+    <Provider store={store}> 
+<div className="App">
+
+<Router>
+
+  <Routes>
+    <Route path="/check-your-magic-number" element={<MagicNumberComp/>} />
+    </Routes>
+</Router>
+
+</div>
+    </Provider>
+    
+  );
+}
+
+export default App;
